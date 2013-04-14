a2546b4d1384c7d58082e44dac2659f28c19661d
Ratna Halder <ratna@mpower-health.com>
2013-04-14 18:13:14 -0400

    Thread constructor is created

--- src/com/ratna/android/droidz/thread/MainThread.java
+++ src/com/ratna/android/droidz/thread/MainThread.java
@@ -1,8 +1,26 @@
 package com.ratna.android.droidz.thread;
 
+import com.ratna.android.droidz.view.MainGamePanel;
+
+import android.view.SurfaceHolder;
+
 public class MainThread extends Thread{
 private boolean running;
+private SurfaceHolder surfaceHolder;
+private MainGamePanel mainGamePanel;
 
+public SurfaceHolder getSurfaceHolder() {
+	return surfaceHolder;
+}
+public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
+	this.surfaceHolder = surfaceHolder;
+}
+public MainGamePanel getMainGamePanel() {
+	return mainGamePanel;
+}
+public void setMainGamePanel(MainGamePanel mainGamePanel) {
+	this.mainGamePanel = mainGamePanel;
+}
 		public void setRunning(boolean running){
 		this.running = running;
 	}
