aff2e8dafe12948d4cd1d39aa4e78f0d39c84a64
Ratna Halder <ratna@mpower-health.com>
2013-04-15 13:16:12 -0400

    touched field is added to keep track of droid

--- src/com/ratna/android/droidz/model/Droidz.java
+++ src/com/ratna/android/droidz/model/Droidz.java
@@ -1,12 +1,14 @@
 package com.ratna.android.droidz.model;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 public class Droidz {
 	
-private Bitmap bitmap;
-private int x;
-private int y;
+private Bitmap bitmap;//the actual bitmap
+private int x; // the x coordinate
+private int y; //the y coordinate
+private boolean touched; //if droidz is touched/picked up
 
 public Droidz(Bitmap bitmap, int x, int y) {
 	super();
@@ -38,4 +40,28 @@ public int getY() {
 public void setY(int y) {
 	this.y = y;
 }
+
+public boolean isTouched() {
+	return touched;
 }
+
+public void setTouched(boolean touched) {
+	this.touched = touched;
+}
+public void draw(Canvas canvas){
+	canvas.drawBitmap(bitmap, x - (bitmap.getWidth()/2), y - (bitmap.getHeight()/2), null);
+}
+public void handleActionDown(int eventX, int eventY){
+	if (eventX >= (x - bitmap.getWidth() / 2) && (eventX <= (x + bitmap.getWidth()/2))) {
+		   if (eventY >= (y - bitmap.getHeight() / 2) && (y <= (y + bitmap.getHeight() / 2))) {
+		    // droid touched
+		    setTouched(true);
+		   } else {
+		    setTouched(false);
+		   }
+		  } else {
+		   setTouched(false);
+		  }
+}
+}
+
